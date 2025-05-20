07776f252b44abd80fea0f8c634c0d8eed94fdd6
Lucas Varela <lucassvarela@gmail.com>
2025-05-20 19:53:28 -0300

    Tablas Categoria y Pago

--- Creacion_BD/Creacion.sql
+++ Creacion_BD/Creacion.sql
@@ -29,6 +29,7 @@ IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G1
 			nombre VARCHAR(50),
 			apellido VARCHAR(50),
 			fecha_nac DATE,
+			email VARCHAR(50),
 			telefono VARCHAR(20),
 			telefono_aux VARCHAR(20)
 		);
@@ -39,3 +40,38 @@ ELSE
 		PRINT 'La tabla Socio ya existe.';
 	END;
 go
+
+-- TABLA CATEGORIA
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Categoria') AND type = N'U') 
+	BEGIN
+		CREATE TABLE psn.Categoria (
+			cod_categoria INT IDENTITY(1,1) PRIMARY KEY,
+			nombre VARCHAR(50),
+			costo DECIMAL(10,2)
+		);
+		PRINT 'Tabla Categoria creada correctamente.';
+	END
+ELSE
+	BEGIN
+		PRINT 'La tabla Categoria ya existe.';
+	END;
+go
+
+-- TABLA PAGO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Pago') AND type = N'U') 
+	BEGIN
+		CREATE TABLE psn.Pago (
+			cod_pago INT IDENTITY(1,1) PRIMARY KEY,
+			monto DECIMAL(10,2),
+			fecha DATE			
+		);
+		PRINT 'Tabla Pago creada correctamente.';
+	END
+ELSE
+	BEGIN
+		PRINT 'La tabla Pago ya existe.';
+	END;
+go
+

41d48a99053d406c9744680f29979157125b7311
Lucas Varela <lucassvarela@gmail.com>
2025-05-20 19:27:37 -0300

    Se añade archivo Creacion.sql

---
+++ Creacion_BD/Creacion.sql
@@ -0,0 +1,12 @@
+--Crea la Base de datos
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Com5600G11')
+	BEGIN
+		CREATE DATABASE Com5600G11;
+		PRINT 'Base de datos creada exitosamente';
+	END;
+go
+
+USE Com5600G11
+go
+
+

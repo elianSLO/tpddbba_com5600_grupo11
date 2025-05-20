f81a66b8eca7e3c05e0b4c38c62ff1204d12f096
Lucas Varela <lucassvarela@gmail.com>
2025-05-20 19:40:01 -0300

    Creacion tabla Socio

--- Creacion_BD/Creacion.sql
+++ Creacion_BD/Creacion.sql
@@ -18,3 +18,24 @@ IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'psn')
 go
 
 --Creacion de tablas
+
+-- TABLA SOCIO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Socio') AND type = N'U') -- 'U' tabla creada por el usuario 'N' es q sea unicode
+	BEGIN
+		CREATE TABLE psn.Socio (
+			numero INT IDENTITY(1,1) PRIMARY KEY,
+			dni INT UNIQUE, 
+			nombre VARCHAR(50),
+			apellido VARCHAR(50),
+			fecha_nac DATE,
+			telefono VARCHAR(20),
+			telefono_aux VARCHAR(20)
+		);
+		PRINT 'Tabla Socio creada correctamente.';
+	END
+ELSE
+	BEGIN
+		PRINT 'La tabla Socio ya existe.';
+	END;
+go

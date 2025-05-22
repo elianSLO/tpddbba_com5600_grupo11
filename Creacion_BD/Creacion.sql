7b89dc3f56a373b70ecd81c880107110dc9da7c1
unknown <modica.elian@gmail.com>
2025-05-22 15:48:34 -0300

    Revision de tablas y claves

--- Creacion_BD/Creacion.sql
+++ Creacion_BD/Creacion.sql
@@ -24,24 +24,23 @@ go
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Socio') AND type = N'U') -- 'U' tabla creada por el usuario 'N' es que sea unicode
 	BEGIN
 		CREATE TABLE psn.Socio (
-			cod_socio INT IDENTITY(1,1) PRIMARY KEY,
-			dni char(8) UNIQUE,
-			nombre VARCHAR(50),
-			apellido VARCHAR(50),
-			fecha_nac DATE,
-			email VARCHAR(100),
-			tel VARCHAR(15),
-			tel_emerg VARCHAR(15),
-			estado BIT, -- 1 - Habilitado, 0 - No habilitado (Pago atrasado o impago)
-			saldo DECIMAL(10,2),
-			nombre_cobertura varchar(50),
-			nro_afiliado varchar(50),
-			tel_cobertura  varchar(15),
-
-			constraint ck_tel check (
-				tel NOT LIKE '%[^0-9]%' and		-- Solo numeros.
-				LEN(tel) between 10 and 14		-- 2 a 4 digitos para prefijo + 6 a 8 para numero / 0800 incluidos
-			)
+			cod_socio			INT IDENTITY(1,1) PRIMARY KEY,
+			dni					CHAR(8) UNIQUE,
+			nombre				VARCHAR(50),
+			apellido			VARCHAR(50),
+			fecha_nac			DATE,
+			email				VARCHAR(100),
+			tel					VARCHAR(15) check (tel NOT LIKE '%[^0-9]%' and		
+													LEN(tel) between 10 and 14),
+			tel_emerg			VARCHAR(15) check (tel_emerg NOT LIKE '%[^0-9]%' and		
+													LEN(tel_emerg) between 10 and 14),
+			estado				BIT, -- 1 - Habilitado, 0 - No habilitado (Pago atrasado o impago)
+			saldo				DECIMAL(10,2),
+			nombre_cobertura	VARCHAR(50),
+			nro_afiliado		VARCHAR(50),
+			tel_cobertura		VARCHAR(15) check (tel_cobertura NOT LIKE '%[^0-9]%' and		
+													LEN(tel_cobertura) between 10 and 14),
+			cod_responsable		INT,
 		);
 		PRINT 'Tabla Socio creada correctamente.';
 	END
@@ -51,22 +50,48 @@ ELSE
 	END;
 go
 
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Invitado') AND type = N'U') -- 'U' tabla creada por el usuario 'N' es que sea unicode
+	BEGIN
+		CREATE TABLE psn.Invitado (
+			cod_invitado		INT IDENTITY(1,1) PRIMARY KEY,
+			dni					CHAR(8) UNIQUE,
+			nombre				VARCHAR(50),
+			apellido			VARCHAR(50),
+			fecha_nac			DATE,
+			email				VARCHAR(100),
+			tel					VARCHAR(15) check (tel NOT LIKE '%[^0-9]%' and		
+													LEN(tel) between 10 and 14),
+			tel_emerg			VARCHAR(15) check (tel_emerg NOT LIKE '%[^0-9]%' and		
+													LEN(tel_emerg) between 10 and 14),
+			estado				BIT, -- 1 - Habilitado, 0 - No habilitado (Pago atrasado o impago)
+			saldo				DECIMAL(10,2),
+			nombre_cobertura	VARCHAR(50),
+			nro_afiliado		VARCHAR(50),
+			tel_cobertura		VARCHAR(15) check (tel_cobertura NOT LIKE '%[^0-9]%' and		
+													LEN(tel_cobertura) between 10 and 14),		
+			cod_responsable		INT,
+
+		);
+		PRINT 'Tabla Invitado creada correctamente.';
+	END
+ELSE
+	BEGIN
+		PRINT 'La tabla Invitado ya existe.';
+	END;
+go
+
 -- TABLA PROFESOR
 
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Profesor') AND type = N'U')
 	BEGIN
 		CREATE TABLE psn.Profesor (
-			cod_prof INT IDENTITY(1,1) PRIMARY KEY,
-			dni char(8) UNIQUE, 
-			nombre VARCHAR(50),
-			apellido VARCHAR(50),
-			email VARCHAR(100),
-			tel varchar(15),
-
-			constraint ck_tel check (
-				tel NOT LIKE '%[^0-9]%' and		-- Solo numeros.
-				LEN(tel) between 10 and 14		-- 2 a 4 digitos para prefijo + 6 a 8 para numero / 0800 incluidos
-			)
+			cod_prof			INT IDENTITY(1,1) PRIMARY KEY,
+			dni					CHAR(8) UNIQUE, 
+			nombre				VARCHAR(50),
+			apellido			VARCHAR(50),
+			email				VARCHAR(100),
+			tel					VARCHAR(15) check (	tel NOT LIKE '%[^0-9]%' and		-- Solo numeros.
+													LEN(tel) between 10 and 14)		-- 2 a 4 digitos para prefijo + 6 a 8 para numero / 0800 incluidos
 		);
 		PRINT 'Tabla Profesor creada correctamente.';
 	END
@@ -81,20 +106,16 @@ go
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Responsable') AND type = N'U')
 	BEGIN
 		CREATE TABLE psn.Responsable (
-			cod_responsable INT IDENTITY(1,1) PRIMARY KEY,
-			dni char(8) UNIQUE, 
-			nombre VARCHAR(50),
-			apellido VARCHAR(50),
-			email VARCHAR(100),
-			parentezco VARCHAR(50),
-			fecha_nac DATE,
-			nro_socio INT, --VER
-			tel varchar(15),
-
-			constraint ck_tel check (
-				tel NOT LIKE '%[^0-9]%' and		-- Solo numeros.
-				LEN(tel) between 10 and 14		-- 2 a 4 digitos para prefijo + 6 a 8 para numero / 0800 incluidos
-			)
+			cod_responsable		INT IDENTITY(1,1) PRIMARY KEY,
+			dni					CHAR(8) UNIQUE, 
+			nombre				VARCHAR(50),
+			apellido			VARCHAR(50),
+			email				VARCHAR(100),
+			parentezco			VARCHAR(50),
+			fecha_nac			DATE,
+			nro_socio			INT,				--VER
+			tel					VARCHAR(15) check (	tel NOT LIKE '%[^0-9]%' and		-- Solo numeros.
+													LEN(tel) between 10 and 14)		-- 2 a 4 digitos para prefijo + 6 a 8 para numero / 0800 incluidos
 		);
 		PRINT 'Tabla Responsable creada correctamente.';
 	END
@@ -110,12 +131,12 @@ go
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Categoria') AND type = N'U') 
 	BEGIN
 		CREATE TABLE psn.Categoria (
-			cod_categoria INT PRIMARY KEY,
-			descripcion VARCHAR(50),
-			valor_mensual DECIMAL(10,2),
-			vig_valor_mens DATE,
-			valor_anual DECIMAL(10,2),
-			vig_valor_anual DATE
+			cod_categoria		INT PRIMARY KEY,
+			descripcion			VARCHAR(50),
+			valor_mensual		DECIMAL(10,2),
+			vig_valor_mens		DATE,
+			valor_anual			DECIMAL(10,2),
+			vig_valor_anual		DATE
 		);
 		PRINT 'Tabla Categoria creada correctamente.';
 	END
@@ -124,6 +145,27 @@ ELSE
 		PRINT 'La tabla Categoria ya existe.';
 	END;
 go
+
+
+-- TABLA SUSCRIPCION
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Suscripcion') AND type = N'U') 
+	BEGIN
+		CREATE TABLE psn.Suscripcion (
+		fecha_suscripcion	DATE,
+		fecha_vto			DATE,
+		cod_socio			INT,
+		cod_categoria		INT,
+		constraint pk_suscripcion primary key (fecha_suscripcion,cod_socio,cod_categoria),
+		);
+		PRINT 'Tabla Suscripcion creada correctamente.';
+	END
+ELSE
+	BEGIN
+		PRINT 'La tabla Suscripcion ya existe.';
+	END;
+go
+
 
 -- TABLA ACTIVIDAD
 
@@ -143,32 +185,18 @@ ELSE
 	END;
 go
 
--- TABLA SUSCRIPCION
-
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Suscripcion') AND type = N'U') 
-	BEGIN
-		CREATE TABLE psn.Suscripcion (
-		fecha_suscripcion DATE PRIMARY KEY,
-		fecha_vto DATE
-		--RELACION N-N ENTRE SOCIO Y CATEGORIA. FALTA AGREGAR PKs
-		);
-		PRINT 'Tabla Suscripcion creada correctamente.';
-	END
-ELSE
-	BEGIN
-		PRINT 'La tabla Suscripcion ya existe.';
-	END;
-go
 
 -- TABLA PAGO
 
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Pago') AND type = N'U') 
 	BEGIN
 		CREATE TABLE psn.Pago (
-			cod_pago INT IDENTITY(1,1) PRIMARY KEY,
-			monto DECIMAL(10,2),
-			fecha_Pago DATE,
-			estado VARCHAR(15)
+			cod_pago		INT IDENTITY(1,1) PRIMARY KEY,
+			monto			DECIMAL(10,2),
+			fecha_pago		DATE,
+			estado			VARCHAR(15),
+			cod_socio		INT,
+			cod_invitado	INT
 		);
 		PRINT 'Tabla Pago creada correctamente.';
 	END
@@ -183,13 +211,14 @@ go
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Factura') AND type = N'U') 
 	BEGIN
 		CREATE TABLE psn.Factura (
-			codFactura INT IDENTITY(1,1) PRIMARY KEY,
-			monto DECIMAL(10,2),
-			fecha_emision DATE,
-			fecha_vto DATE,
-			fecha_seg_vto DATE,
-			recargo DECIMAL(10,2),
-			estado VARCHAR(10) CHECK (estado IN ('Pendiente', 'Pagada'))	
+			codFactura		INT IDENTITY(1,1) PRIMARY KEY,
+			monto			DECIMAL(10,2),
+			fecha_emision	DATE,
+			fecha_vto		DATE,
+			fecha_seg_vto	DATE,
+			recargo			DECIMAL(10,2),
+			estado			VARCHAR(10) CHECK (estado IN ('Pendiente', 'Pagada','Vencida')),
+			cod_socio		INT
 		); 
 		PRINT 'Tabla Factura creada correctamente.';
 	END
@@ -204,11 +233,11 @@ go
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Reembolso') AND type = N'U') 
 	BEGIN
 		CREATE TABLE psn.Reembolso (
-			codReembolso INT IDENTITY(1,1) PRIMARY KEY,
-			monto DECIMAL(10,2),
-			medio_Pago VARCHAR(50),
-			fecha DATE,
-			motivo VARCHAR(50)
+			codReembolso		INT IDENTITY(1,1) PRIMARY KEY,
+			monto				DECIMAL(10,2),
+			medio_Pago			VARCHAR(50),
+			fecha				DATE,
+			motivo				VARCHAR(50)
 		);
 		PRINT 'Tabla Reembolso creada correctamente.';
 	END
@@ -223,11 +252,13 @@ go
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Reserva') AND type = N'U') 
 	BEGIN
 		CREATE TABLE psn.Reserva (
-			codReserva INT IDENTITY(1,1) PRIMARY KEY,
-			monto DECIMAL(10,2),
-			fechahoraInicio DATETIME,	
-			fechahoraFin DATETIME,
-			piletaSUMcolonia VARCHAR(50)
+			codReserva			INT IDENTITY(1,1) PRIMARY KEY,
+			monto				DECIMAL(10,2),
+			fechahoraInicio		DATETIME,	
+			fechahoraFin		DATETIME,
+			piletaSUMcolonia	VARCHAR(50),
+			cod_socio			INT,
+			cod_invitado		INT
 		);
 		PRINT 'Tabla Reserva creada correctamente.';
 	END
@@ -242,9 +273,9 @@ go
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Clase') AND type = N'U') 
 	BEGIN
 		CREATE TABLE psn.Clase (
-		codClase INT IDENTITY (1,1) PRIMARY KEY,
-		categoria VARCHAR(50),
-		-- AGREGAR FKs 
+		cod_clase			INT IDENTITY (1,1) PRIMARY KEY,
+		categoria			INT NOT NULL,
+		cod_actividad		INT NOT NULL,
 		);
 		PRINT 'Tabla Clase creada correctamente.';
 	END
@@ -259,9 +290,10 @@ go
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Inscripto') AND type = N'U') 
 	BEGIN
 		CREATE TABLE psn.Inscripto (
-		fecha_inscripcion DATE PRIMARY KEY,
-		estado VARCHAR(50),
-		--RELACION N-N ENTRE SOCIO Y CLASE. FALTA AGREGAR PKs
+		fecha_inscripcion		DATE NOT NULL,
+		estado					VARCHAR(50),
+		cod_socio				INT NOT NULL,
+		cod_clase				INT NOT NULL
 		);
 		PRINT 'Tabla Inscripto creada correctamente.';
 	END
@@ -278,8 +310,9 @@ go
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Com5600G11.psn.Asiste') AND type = N'U') 
 	BEGIN
 		CREATE TABLE psn.Asiste (
-		fecha DATE PRIMARY KEY,
-		--RELACION N-N ENTRE SOCIO Y CLASE. FALTA AGREGAR PKs
+		fecha		DATE NOT NULL,
+		cod_socio	INT NOT NULL,
+		cod_clase	INT NOT NULL
 		);
 		PRINT 'Tabla Asiste creada correctamente.';
 	END
@@ -287,4 +320,38 @@ ELSE
 	BEGIN
 		PRINT 'La tabla Asiste ya existe.';
 	END;
+go
+
+-- Una vez creadas todas las tablas, defino claves.
+
+alter table psn.Socio add constraint fk_responsable FOREIGN KEY (cod_responsable) references psn.Responsable(cod_responsable);
+
+alter table psn.Suscripcion add 		
+	constraint fk_socio_sus foreign key	(cod_socio) references psn.Socio(cod_socio),
+	constraint fk_cat_sus foreign key (cod_categoria) references psn.Categoria(cod_categoria);
+
+alter table psn.Pago add 		
+	constraint fk_socio_pago foreign key (cod_socio) references psn.Socio(cod_socio),
+	constraint fk_invit_pago foreign key (cod_invitado) references psn.Invitado(cod_invitado);
+
+alter table psn.Factura add 		
+	constraint fk_socio_fact foreign key (cod_socio) references psn.Socio(cod_socio);
+
+alter table psn.Reserva add 		
+	constraint fk_socio_res foreign key (cod_socio) references psn.Socio(cod_socio),
+	constraint fk_invit_res foreign key (cod_invitado) references psn.Invitado(cod_invitado);
+
+alter table psn.Clase add 		
+	constraint fk_act_clase foreign key (cod_actividad) references psn.Actividad(cod_actividad);
+
+alter table psn.Inscripto add 	
+	constraint pk_inscripto primary key (fecha_inscripcion,cod_socio,cod_clase),
+	constraint fk_socio_inscripto foreign key (cod_socio) references psn.Socio(cod_socio),
+	constraint fk_clase_inscripto foreign key (cod_clase) references psn.Clase(cod_clase);
+
+alter table psn.Asiste add 	
+constraint pk_asiste primary key (fecha,cod_socio,cod_clase),
+constraint fk_socio_asiste foreign key (cod_socio) references psn.Socio(cod_socio),
+constraint fk_clase_asiste foreign key (cod_clase) references psn.Clase(cod_clase);
+
 go

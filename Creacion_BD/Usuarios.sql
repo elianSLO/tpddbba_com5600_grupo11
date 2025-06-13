343e7951894db8b5ad344334e9d699979267f369
unknown <modica.elian@gmail.com>
2025-06-13 14:45:22 -0300

    Ultima entrega y usuarios db_owner

---
+++ Creacion_BD/Usuarios.sql
@@ -0,0 +1,37 @@
+SELECT name, state_desc 
+FROM sys.databases;
+
+use Com5600G11
+go
+CREATE LOGIN usuario_test
+WITH PASSWORD = '1234', DEFAULT_DATABASE = Com5600G11,
+CHECK_POLICY = OFF, CHECK_EXPIRATION = OFF ;
+
+CREATE LOGIN elian
+WITH PASSWORD = 'elian', DEFAULT_DATABASE = Com5600G11,
+CHECK_POLICY = OFF, CHECK_EXPIRATION = OFF ;
+
+CREATE LOGIN lucas
+WITH PASSWORD = 'lucas', DEFAULT_DATABASE = Com5600G11,
+CHECK_POLICY = OFF, CHECK_EXPIRATION = OFF ;
+
+CREATE LOGIN matias
+WITH PASSWORD = 'matias', DEFAULT_DATABASE = Com5600G11,
+CHECK_POLICY = OFF, CHECK_EXPIRATION = OFF ;
+
+CREATE LOGIN josue
+WITH PASSWORD = 'josue', DEFAULT_DATABASE = Com5600G11,
+CHECK_POLICY = OFF, CHECK_EXPIRATION = OFF ;
+
+--create user usuario_test for LOGIN usuario_test
+create user elian for LOGIN elian
+create user lucas for LOGIN lucas
+create user matias for LOGIN matias
+create user josue for LOGIN josue
+
+alter role db_owner add member elian
+alter role db_owner add member lucas
+alter role db_owner add member matias
+alter role db_owner add member josue
+
+select name from sys.sql_logins

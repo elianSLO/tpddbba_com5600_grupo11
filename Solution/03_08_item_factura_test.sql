3e720ae7613078ba93a9d9f36688789f66b94f94
unknown <modica.elian@gmail.com>
2025-07-03 11:00:54 -0300

    Test Item Factura OK

--- Solution/03_08_item_factura_test.sql
+++ Solution/03_08_item_factura_test.sql
@@ -7,25 +7,25 @@ GO
 
 -- Borrado de tablas
 
-DELETE FROM psn.Socio 
-DELETE FROM psn.Factura
-DELETE FROM psn.Item_Factura
+DELETE FROM Persona.Socio 
+DELETE FROM Finanzas.Factura
+DELETE FROM Finanzas.Item_Factura
 
 
 ------------------------------------------------------------ 1. PRUEBAS insertarItem_Factura
 
-INSERT INTO psn.Socio (cod_socio, nombre, apellido, dni, email, fecha_nac, tel, tel_emerg, nombre_cobertura, nro_afiliado, tel_cobertura, estado, saldo, cod_responsable)
+INSERT INTO Persona.Socio (cod_socio, nombre, apellido, dni, email, fecha_nac, tel, tel_emerg, nombre_cobertura, nro_afiliado, tel_cobertura, estado, saldo, cod_responsable)
 VALUES ('SN-00001', 'Juan', 'Pérez', '12345678', 'juan@example.com', '1990-05-10', '123456789', '987654321', 'OSDE', 'AF123', '1122334455', 1, 0.00, NULL);
 
-INSERT INTO psn.Factura (monto, fecha_emision, fecha_vto, fecha_seg_vto, recargo, estado, cod_socio)
+INSERT INTO Finanzas.Factura (monto, fecha_emision, fecha_vto, fecha_seg_vto, recargo, estado, cod_socio)
 VALUES (2000.00, '2025-06-01', '2025-06-10', '2025-06-20', 0.1, 'Pendiente', 'SN-00001');
 
-SELECT cod_Factura FROM psn.Factura WHERE cod_socio = 'SN-00001';
+SELECT cod_Factura FROM Finanzas.Factura WHERE cod_socio = 'SN-00001';
 
 
 -- CASO 1.1: Inserción correcta.
 
-EXEC stp.insertarItem_factura 
+EXEC Finanzas.insertarItem_factura 
     @cod_item = 1,
     @cod_Factura = 1,
     @monto = 25000.00,
@@ -33,11 +33,11 @@ EXEC stp.insertarItem_factura
 
 -- Verificacion: 
 
-SELECT * FROM psn.Item_Factura
+SELECT * FROM Finanzas.Item_Factura
 
 -- CASO 1.2: Inserción del mismo item (debe dar error)
 
-EXEC stp.insertarItem_factura 
+EXEC Finanzas.insertarItem_factura 
     @cod_item = 1,
     @cod_Factura = 1,
     @monto = 25000.00,
@@ -45,7 +45,7 @@ EXEC stp.insertarItem_factura
 
 -- CASO 1.3: Código de factura inexistente.
 
-EXEC stp.insertarItem_factura 
+EXEC Finanzas.insertarItem_factura 
     @cod_item = 1,
     @cod_Factura = 2,
     @monto = 25000.00,
@@ -53,7 +53,7 @@ EXEC stp.insertarItem_factura
 
 -- CASO 1.4: Monto negativo
 
-EXEC stp.insertarItem_factura 
+EXEC Finanzas.insertarItem_factura 
     @cod_item = 2,
     @cod_Factura = 1,
     @monto = -10000,
@@ -61,7 +61,7 @@ EXEC stp.insertarItem_factura
 
 -- CASO 1.5: Cateogría Vacía 
 
-EXEC stp.insertarItem_factura 
+EXEC Finanzas.insertarItem_factura 
     @cod_item = 2,
     @cod_Factura = 1,
     @monto = 10000,
@@ -69,7 +69,7 @@ EXEC stp.insertarItem_factura
 
 -- CASO 1.6: Categoría NULL
 
-EXEC stp.insertarItem_factura 
+EXEC Finanzas.insertarItem_factura 
     @cod_item = 2,
     @cod_Factura = 1,
     @monto = 10000,
@@ -79,7 +79,7 @@ EXEC stp.insertarItem_factura
 
 -- CASO 2.1 - Modificacion correcta
 
-EXEC stp.modificarItem_Factura 
+EXEC Finanzas.modificarItem_Factura 
     @cod_item = 1,
     @cod_Factura = 1,
     @monto = 15000.00, -- Cambio Precio
@@ -87,7 +87,7 @@ EXEC stp.modificarItem_Factura
 
 -- CASO 2.2 - Modificación factura inexistente
 
-EXEC stp.modificarItem_Factura 
+EXEC Finanzas.modificarItem_Factura 
     @cod_item = 1,
     @cod_Factura = 2,
     @monto = 15000.00,
@@ -95,7 +95,7 @@ EXEC stp.modificarItem_Factura
 
 -- CASO 2.3 - Modificación con Monto negativo
 
-EXEC stp.modificarItem_Factura 
+EXEC Finanzas.modificarItem_Factura 
     @cod_item = 1,
     @cod_Factura = 1,
     @monto = -15000.00, 
@@ -103,7 +103,7 @@ EXEC stp.modificarItem_Factura
 
 -- CASO 2.4 - Modificación con Categoría vacía
 
-EXEC stp.modificarItem_Factura 
+EXEC Finanzas.modificarItem_Factura 
     @cod_item = 1,
     @cod_Factura = 1,
     @monto = 15000.00, 
@@ -111,7 +111,7 @@ EXEC stp.modificarItem_Factura
 
 -- CASO 2.5 - Modificación con Cateogría nula
 
-EXEC stp.modificarItem_Factura 
+EXEC Finanzas.modificarItem_Factura 
     @cod_item = 1,
     @cod_Factura = 1,
     @monto = 15000.00, 
@@ -121,9 +121,9 @@ EXEC stp.modificarItem_Factura
 
 -- Borro el item 1
 
-EXEC stp.borrarItem_factura @cod_item = 1, @cod_factura = 1
+EXEC Finanzas.borrarItem_factura @cod_item = 1, @cod_factura = 1
 
 -- Verifico que el item no existe.
 
-EXEC stp.borrarItem_factura @cod_item = 1, @cod_factura = 1 
+EXEC Finanzas.borrarItem_factura @cod_item = 1, @cod_factura = 1 
 
